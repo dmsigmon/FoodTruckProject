7a01063896ece44a0002ff58d4a538b10218bfac
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-09-04 23:37:15 -0600

    put most of main in a method and fixed highest rating issue

--- src/com/skilldistillery/foodproject/FoodTruckApp.java
+++ src/com/skilldistillery/foodproject/FoodTruckApp.java
@@ -8,47 +8,54 @@ public class FoodTruckApp {
 
 		FoodTruck[] fTArr = new FoodTruck[5];
 
-		Scanner kb = new Scanner(System.in);
 
 		double cumulativeRatings = 0;
 		double avgRating = 0.0;
 		int highestRating = 0;
 		String bestFoodTruck = "";
+		
+		createAndListFoodTruck(fTArr,cumulativeRatings, avgRating, highestRating, bestFoodTruck);
+		
+	}
 
+	private static void createAndListFoodTruck(FoodTruck[] fTArr,double cumulativeRatings, double avgRating, int highestRating, String bestFoodTruck) {
+		Scanner kb = new Scanner(System.in);
+		
 		for (int i = 0; i < 5; i++) {
-
+			
 			FoodTruck foodTruck = new FoodTruck(0, null, null, i);
-
+			
 			System.out.println(" You can enter up to 5 different food trucks (type Quit for food truck name to exit");
-
+			
 			System.out.println("What is the name of the food truck?");
 			foodTruck.setName(kb.next());
-
+			
 			if (foodTruck.getName().equals("quit")) {
 				break;
 			}
-			// add if clause to break loop if name is "quit"
-
+			
+			
 			System.out.println("What is the food type of the food truck?");
 			foodTruck.setFoodType(kb.next());
-
+			
 			System.out.println("What is the rating of the food truck between [1-5]?");
 			foodTruck.setRating(kb.nextInt());
-
+			
 			cumulativeRatings += foodTruck.getRating();
-
+			
 			avgRating = cumulativeRatings / (i + 1);
-
+			
 			foodTruck.setUniqueId(i);
-
+			
 			fTArr[i] = foodTruck;
-
+			
 			if (highestRating < foodTruck.getRating()) {
+				highestRating = foodTruck.getRating();
 				bestFoodTruck = fTArr[i].toString();
-
+				
 			}
 		}
-
+		
 		boolean flag = true;
 		while (flag) {
 			System.out.println("-------------------------------------------");
@@ -59,7 +66,7 @@ public class FoodTruckApp {
 			System.out.println("(4) Quit the Program                       ");
 			System.out.println("-------------------------------------------");
 			int menuOption = kb.nextInt();
-
+			
 			if (menuOption == 1) {
 				for (int i = 0; i < fTArr.length; i++) {
 					if (fTArr[i] == null) {
